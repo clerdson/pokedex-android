7b88b3f0da56ce8f8dd7a9716e1915cbe6c0148b
clerdson <clerdson@gmail.com>
2022-08-11 17:13:26 -0300

    service

---
+++ app/src/main/java/com/example/pokedex/ApiService.kt
@@ -0,0 +1,23 @@
+package com.example.pokedex
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface ApiService {
+  @GET("pokedex.json")
+    suspend fun getPokemon():Pokehub
+    companion object{
+
+    var apiService:ApiService? = null
+      fun getInstance():ApiService{
+        if (apiService == null){
+          apiService = Retrofit.Builder()
+            .baseUrl("https://raw.githubusercontent.com/Biuni/PokemonGo-Pokedex/master")
+            .addConverterFactory(GsonConverterFactory.create()).
+            build().create(ApiService::class.java)
+        }
+        return apiService!!
+
+      }      }
+}
---
+++ app/src/main/java/com/example/pokedex/Pokehub.kt
@@ -0,0 +1,8 @@
+package com.example.pokedex
+
+class Pokehub (
+   val pokemon:List<Pokemon>
+        )
+class Pokemon(
+    val id:Int,
+    val name:String)
